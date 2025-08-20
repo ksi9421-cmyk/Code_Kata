881bac822f8700e0bd44a8a6cab419ac1e0623db
ksi9421-cmyk <ksi9421@gmail.com>
2025-08-20 09:39:49 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer;
+    
+    if(0<angle && angle<90)
+    {
+        answer=1;
+    }
+    if(angle == 90)
+    {
+        answer=2;
+    }
+    if(90<angle && angle<180)
+    {
+        answer=3;
+    }
+    if(angle == 180)
+    {
+        answer=4;
+    }
+    return answer;
+}
