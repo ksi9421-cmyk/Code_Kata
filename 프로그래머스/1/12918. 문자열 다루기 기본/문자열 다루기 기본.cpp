d2a99988cb789ba305f635b1430af482f99354ff
Kwon IK Hwan <ksi9421@gmail.com>
2025-09-19 09:44:05 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    
+    bool answer = true;
+    
+    if(s.size()!=4 && s.size()!=6)
+    {
+        answer = false;
+    }
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]<'0' || '9'<s[i])
+        {
+            answer = false;
+        }
+    }
+    return answer;
+}
