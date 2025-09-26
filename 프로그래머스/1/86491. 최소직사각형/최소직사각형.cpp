9bae0195978f9aaf4a915ff158b06d12811d6011
Kwon IK Hwan <ksi9421@gmail.com>
2025-09-26 09:43:16 +0900

    [level 1] Title: 최소직사각형, Time: 0.68 ms, Memory: 5.03 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int w=0;
+    int h=0;
+    
+    for(int i=0; i<sizes.size(); i++)
+    {
+       if(sizes[i][0]<sizes[i][1])
+       {
+           swap(sizes[i][0],sizes[i][1]);
+       }
+        w = max(w, sizes[i][0]);
+        h = max(h, sizes[i][1]);
+    }
+    
+    return w*h;
+}
+
